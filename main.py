b84bb5da6ccfcaa081fe1bf24202b55582875a5e
Simonxu <q2798573282@gmail.com>
2025-11-16 17:15:22 +0800

    update on 2025-11-16_17:15:22

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ if __name__ == '__main__':
     # else:
     #     print(f"[{datetime.datetime.now()}] 启动单次执行模式")
     #     job()
-    work_flow.prepare(today='2025-11-11', today_ymd='20251111')
+    work_flow.prepare(today='2025-11-14', today_ymd='20251114')
