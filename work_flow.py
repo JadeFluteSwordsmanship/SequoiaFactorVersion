6425a746aae8a0c0b58674bafd383ff92d88aa2d
Simonxu <q2798573282@gmail.com>
2025-12-16 22:30:24 +0800

    update on 2025-12-16_22:30:24

--- work_flow.py
+++ work_flow.py
@@ -11,6 +11,7 @@ from strategy import low_backtrace_increase
 from strategy import keep_increasing
 from strategy import high_tight_flag
 import akshare as ak
+import data_reader
 import push
 import logging
 import time
@@ -19,6 +20,12 @@ import daily_data_fetcher
 from factors.utils import update_all_factors_daily
 from datetime import datetime
 import push
+
+num_map = {
+    1: '首', 2: '二', 3: '三', 4: '四', 5: '五', 
+    6: '六', 7: '七', 8: '八', 9: '九', 10: '十',
+    11: '十一', 12: '十二', 13: '十三'
+}
 
 def prepare_old():
     logging.info("************************ process start ***************************************")
@@ -128,13 +135,45 @@ def push_result(today=None, today_ymd=None):
     codes = df['code'].tolist()
     stocks = factor.read_stock_basic_data(codes,None,None)
     daily_basic = factor.read_daily_basic_data(codes,today,1)
+    df_daily_basic = data_reader.get_daily_basic_data(data_reader.list_available_stocks('daily'),today,15)
+    df_daily_basic['limit_status'] = df_daily_basic['limit_status'].fillna(0)
+    df_sorted = df_daily_basic.sort_values(by=['stock_code', 'trade_date'], ascending=[True, False])
+    streak_series = df_sorted.groupby('stock_code').apply(count_consecutive_limit_up)
+    result_df = streak_series.reset_index(name='limit_up_count')
+    final_df = result_df[result_df['limit_up_count'] > 0].sort_values(by=['limit_up_count'], ascending=[False])
+    stocks_limit = factor.read_stock_basic_data(final_df['stock_code'].tolist(),None,None)
     merge = df.merge(stocks, right_on='stock_code', left_on='code', how='left')
     merge = merge.merge(daily_basic, right_on='stock_code', left_on='code', how='left')
+    merge2 = stocks_limit.merge(final_df,on='stock_code',how='left')
+    merge2 = merge2[merge2['list_status']=='L']
+    merge2['stock_label'] = merge2['stock_code'] + ' ' + merge2['name'] + '[' + merge2['industry'] + ']'
     msg = "以下股票可能有反弹，可关注：\n"
     for index, row in merge.iterrows():
         if row['turnover_rate'] > 2:
             msg += f"{row['code']} {row['name']} {'（跌停）' if row['limit_status']== -1 else ('（涨停）' if row['limit_status']== 1 else '')} 现价：{row['close']:.2f}\n"
+    msg+="-----今日连板梯队-----\n"
+    series = merge2.groupby('limit_up_count')['stock_label'].apply(list).sort_index(ascending=False)
+    for count, labels in series.items():
+        cn_count = num_map.get(count, str(count))
+        content = ', '.join(labels)
+        msg+=f"{cn_count}连板：{content}" if count>1 else f"{cn_count}板：{content}"
+        msg+="\n"
+
     push.push(msg, today=today)
+
+def count_consecutive_limit_up(group):
+    # 获取该股票最近这几天的 limit_status
+    statuses = group['limit_status'].values
+    
+    streak = 0
+    for status in statuses:
+        if status == 1:
+            streak += 1
+        else:
+            # 一旦遇到不是涨停的，立即停止计数（连板中断）
+            break
+            
+    return streak
 
 if __name__ == '__main__':
     push_result(today='2025-11-05')
